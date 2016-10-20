07007c286077cd06b451916c6839e49aa5fe79ce
sebastienquiqure <sebastien.quiquerez@he-arc.ch>
2016-10-20 09:29:00 +0200

    push fatidique

---
+++ src/java/ch/hearc/ig/odi/peoplelist/business/Person.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.hearc.ig.odi.peoplelist.business;
+
+import java.util.Date;
+
+/**
+ *
+ * @author sebastie.quiquere
+ */
+public class Person {
+    
+    private Long id;
+    private String gender;
+    private String firstName;
+    private String lastName;
+    private boolean married;
+    private Date birthDate;
+
+    public Person() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public boolean isMarried() {
+        return married;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lasteName) {
+        this.lastName = lasteName;
+    }
+
+    public void setMarried(Boolean married) {
+        this.married = married;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+    
+    
+    
+}
